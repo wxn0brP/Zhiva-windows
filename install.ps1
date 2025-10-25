776982a9379831ca26b60713d2aec5f7ca148932
wxn0brP <toja.filiper@gmail.com>
2025-10-25 14:10:56 +0200

    fix: logs

--- install.ps1
+++ install.ps1
@@ -5,15 +5,16 @@ if (-not (Test-Path $ZhivaFolder)) {
     
     $BootstrapUrl = "https://github.com/wxn0brP/Zhiva-windows/releases/download/native/zhiva-bootstrap.exe"
     $TempFile = Join-Path $env:TEMP "zhiva-bootstrap.exe"
+    Write-Host "[Z-WIN-2-02] Bootstrap temp file: $TempFile"
 
     Invoke-WebRequest -Uri $BootstrapUrl -OutFile $TempFile
 
-    Write-Host "[Z-WIN-2-02] Running bootstrap..."
+    Write-Host "[Z-WIN-2-03] Running bootstrap..."
     Start-Process -FilePath $TempFile -Wait
     Remove-Item $TempFile -Force
 }
 
-Write-Host "[Z-WIN-2-03] Running zhiva-install..."
+Write-Host "[Z-WIN-2-04] Running zhiva-install..."
 Start-Process "zhiva-install" -ArgumentList "%%name%%" -Wait
 
-Write-Host "[Z-WIN-2-04] Done."
+Write-Host "[Z-WIN-2-05] Done."
