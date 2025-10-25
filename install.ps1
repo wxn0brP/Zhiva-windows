79a7dcbb80fefdb980c83bffa010710a6013c619
wxn0brP <toja.filiper@gmail.com>
2025-10-25 13:47:15 +0200

    feat: install script

--- install.ps1
+++ install.ps1
@@ -1,1 +1,19 @@
-Write-Host "Hello from Zhiva!"
+$ZhivaFolder = Join-Path $env:USERPROFILE ".zhiva"
+
+if (-not (Test-Path $ZhivaFolder)) {
+    Write-Host "[Z-WIN-2-01] .zhiva folder does not exist. Downloading bootstrap..."
+    
+    $BootstrapUrl = "https://github.com/wxn0brP/Zhiva-windows/releases/download/native/zhiva-bootstrap.exe"
+    $TempFile = Join-Path $env:TEMP "zhiva-bootstrap.exe"
+
+    Invoke-WebRequest -Uri $BootstrapUrl -OutFile $TempFile
+
+    Write-Host "[Z-WIN-2-02] Running bootstrap..."
+    Start-Process -FilePath $TempFile -Wait
+    Remove-Item $TempFile -Force
+}
+
+Write-Host "[Z-WIN-2-03] Running zhiva-install..."
+Start-Process "zhiva-install" -ArgumentList "%%name%%" -Wait
+
+Write-Host "[Z-WIN-2-04] Done."
