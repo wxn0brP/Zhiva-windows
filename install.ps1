146d987664aefda10e6b85edc18f6874cf005828
wxn0brP <toja.filiper@gmail.com>
2026-01-05 19:24:10 +0100

    fix: install

--- install.ps1
+++ install.ps1
@@ -4,15 +4,18 @@ Start-Transcript -Path $LogFile -Append -Force
 $ZhivaDir = "$env:USERPROFILE\.zhiva"
 
 if (-not (Test-Path $ZhivaDir)) {
-    Write-Host "[Z-WIN-0-01] .zhiva folder missing. Downloading bootstrap..."
+    Write-Host "[Z-WIN-0-01] .zhiva folder missing. Bootstrapping..."
     irm https://raw.githubusercontent.com/wxn0brP/Zhiva/HEAD/install/prepare.ps1 | iex
+    Write-Host "[Z-WIN-0-02] Restarting script..."
+    Start-Process -FilePath "powershell" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Wait
+    exit
 }
 
 $ZhivaCmd = "$ZhivaDir\bin\zhiva.cmd"
 
-Write-Host "[Z-WIN-0-02] Running zhiva install..."
+Write-Host "[Z-WIN-0-07] Running zhiva install..."
 Start-Process $ZhivaCmd -ArgumentList "self" -Wait
 Start-Process $ZhivaCmd -ArgumentList "install", "%%name%%" -Wait
 
-Write-Host "[Z-WIN-0-03] Done."
+Write-Host "[Z-WIN-0-08] Done."
 Stop-Transcript
