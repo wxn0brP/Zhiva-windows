004aa2c9b5e4664bd240bba2ae6aceb34bb14a9b
wxn0brP <toja.filiper@gmail.com>
2026-01-08 19:15:17 +0100

    fix: install

--- scripts/1.deps.ps1
+++ scripts/1.deps.ps1
@@ -1,13 +1,13 @@
 if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
     Write-Output "[Z-WIN-1-01] Git is not installed. Installing..."
-    winget install --id Git.Git -e --silent --scope user
+    winget install Git.Git
 } else {
     Write-Output "[Z-WIN-1-02] Git is already installed."
 }
 
 if (-not (Get-Command bun -ErrorAction SilentlyContinue)) {
     Write-Output "[Z-WIN-1-03] Bun is not installed. Installing..."
-    winget install --id Oven-sh.Bun -e --silent --scope user
+    winget install Oven-sh.Bun
 } else {
     Write-Output "[Z-WIN-1-04] Bun is already installed."
 }
